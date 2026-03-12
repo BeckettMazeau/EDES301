add6866649ce663bbf80daec38c44623c5402c28
BLMAZ <beckettmazeau@gmail.com>
2026-03-12 11:55:00 -0500

    simple_calc Done
    Completed Simple Calc for HW4

---
+++ Simple Calc/simple_calc.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+--------------------------------------------------------------------------
+Simple Calculator
+--------------------------------------------------------------------------
+License:   
+Copyright 2026 - <Beckett Mazeau>
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Simple calculator that will 
+  - Take in two numbers from the user
+  - Take in an operator from the user
+  - Perform the mathematical operation and provide the number to the user
+  - Repeat
+
+Operations:
+  - "+" : addition
+  - "-" : subtraction
+  - "*" : multiplication
+  - "/" : division
+
+Error conditions:
+  - Invalid operator --> Program should exit
+  - Invalid number   --> Program should exit
+
+--------------------------------------------------------------------------
+"""
+
+# NOTE - Add import statements to allow access to Python library functions
+# NOTE - Hint:  Look at  https://docs.python.org/3/library/operator.html
+import operator
+import re
+from operator import truediv
+
+# ------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------
+
+# NOTE - No constants are needed for this example 
+
+# ------------------------------------------------------------------------
+# Global variables
+# ------------------------------------------------------------------------
+
+# NOTE - Global variable to map an operator string (e.g. "+") to 
+# NOTE - the appropriate function.
+operators = {
+    "+" : operator.add,
+    "-" : operator.sub,
+    "*" : operator.mul,
+    "/" : operator.truediv,
+    "add" : operator.add,
+    "addition" : operator.add,
+    "sub" : operator.sub,
+    "subtract" : operator.sub,
+    "subtraction" : operator.sub,
+    "multiply" : operator.mul,
+    "multiplication" : operator.mul,
+    "div" : operator.truediv,
+    "divide" : operator.truediv,
+    "division" : operator.truediv,
+    "stop":"exit",
+    "exit":"exit",
+    "end": "exit"
+}
+ops_to_symbols = {
+    operator.add: "+",
+    operator.sub: "-",
+    operator.mul: "*",
+    operator.truediv: "/"
+}
+
+
+
+# ------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------
+
+def get_user_input():
+    """ Get input from the user.
+        Returns tuple:  (number, number, function) or 
+                        (None, None, None) if inputs invalid
+    """
+    # NOTE - Use "try"/"except" statements to allow code to handle errors gracefully.      
+    try:
+        novalid_input1= True
+        novalid_input2 = True
+        novalid_input3 = True
+        operation_input = ""
+        number1_input= ""
+        number2_input = ""
+        while novalid_input1:
+            operation_input = input("Please enter the operation to be performed.\n"
+                               "Available operations: \n"+
+                               "Addition: +\n"
+                               "Subtraction: -\n"
+                               "Multiplication: *\n"
+                               "Division: /\n"
+                               "Exit Program: Exit")
+            operation_input = re.sub(r'\d+', '', operation_input.strip().lower())
+            if (operation_input not in operators) or (operation_input == "") or operation_input == None:
+                print("Invalid Input, please try again.\n"
+                      "Reference the attached table for valid inputs.\n")
+                continue
+            if operators[operation_input] == "exit":
+                return("exit",None,None)
+            novalid_input1 = False
+        while (novalid_input2):
+            try:
+                number1_input = float(re.sub(r'[^0-9,.]', '', input("Please enter the first number in the chosen operation.\n: ")))
+            except:
+                print("Invalid Input, please try again.\n")
+                continue
+            if number1_input == "" or number1_input == None:
+                print("Invalid Input, please try again.\n")
+                continue
+            novalid_input2 = False
+        while (novalid_input3):
+            try:
+                number2_input = float(re.sub(r'[^0-9,.]', '', input("Please enter the second number in the chosen operation.\n: ")))
+            except:
+                print("Invalid Input, please try again.\n")
+                continue
+            if number2_input == "" or number2_input == None:
+                print("Invalid Input, please try again.\n")
+                continue
+            novalid_input3 = False
+        return(operation_input, number1_input, number2_input)
+    except:
+        print("Invalid Input")
+        return (None, None, None)
+
+# End def
+
+
+
+# ------------------------------------------------------------------------
+# Main script
+# ------------------------------------------------------------------------
+
+# NOTE - The python variable "__name__" is provided by the language and can 
+# NOTE - be used to determine how the file is being executed.  For example,
+# NOTE - if the program is being executed on the command line:
+# NOTE -   python3 simple_calc.py
+# NOTE - then the "__name__" will be the string:  "__main__".  If the file 
+# NOTE - is being imported into another python file:
+# NOTE -   import simple_calc
+# NOTE - the the "__name__" will be the module name, i.e. the string "simple_calc"
+
+if __name__ == "__main__":
+    run = True
+    while run:
+        operator_input, number1_input, number2_input = get_user_input()
+        op = ""
+        if operators[operator_input] == "exit":
+            run = False
+            print("Exiting...")
+            break
+        op_symbol = ops_to_symbols[operators[operator_input]]
+        value = operators[operator_input](number1_input, number2_input)
+        print(f"\n\nOutput:\n {number1_input} {op_symbol} {number2_input} = {value:.5f}\n")
+
+
